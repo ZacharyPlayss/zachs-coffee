1a1bc097d74631848ab32f5f4cd011f67c142576
Zachary Van De Staey <zachary.vandestaey@student.kdg.be>
2026-07-31 12:35:41 +0200

    fix:updated base to match webflow and removal of react plugins which are non required.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { webcore } from 'webcoreui/integration';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  base:'./',
-  integrations: [react(), webcore()]
+  base:'/app',
+  integrations: [webcore()]
 });
