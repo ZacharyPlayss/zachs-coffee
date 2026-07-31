054a9088cd1ef89edd2945aeea4c533c5b58854f
Zachary Van De Staey <zachary.vandestaey@student.kdg.be>
2026-07-31 13:17:38 +0200

    fix: updated to server built type.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { webcore } from 'webcoreui/integration';
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output:'server',
+  adapter: cloudflare(),
   base:'/app',
   build: {
     assetsPrefix: '/app',
