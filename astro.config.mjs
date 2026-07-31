9f7ed9180544bc74e37e05143407db5d794111e1
Zachary Van De Staey <zachary.vandestaey@student.kdg.be>
2026-07-31 12:53:48 +0200

    feat: configured the assetsprefix for the build.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   base:'/app',
+  build: {
+    assetsPrefix: process.env.ASSETS_PREFIX,
+  },
   integrations: [webcore()]
 });
